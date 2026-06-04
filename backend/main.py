e70b5beb541867945be09526d0a8eca6a400b3c2
ad1tyq <adityabhardwaj9b7@gmail.com>
2026-06-04 14:05:44 +0530

    fixed render issue

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Smart Community Health Monitoring Backend")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://iic-strawhats.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
